1053291b1d2e69a18e5113f57c959824b08455f3
Naveen <naveenramlu28@gmail.com>
2025-08-12 01:47:49 +0530

    #update

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,15 @@
-import type { NextConfig } from "next";
-import type { Configuration, RuleSetRule } from "webpack";
+import { NextConfig } from 'next';
+import { Configuration, RuleSetRule } from 'webpack';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   webpack(config: Configuration): Configuration {
     config.module = config.module ?? {};
-    
     config.module.rules = config.module.rules ?? [];
 
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     } as RuleSetRule);
 
     return config;
